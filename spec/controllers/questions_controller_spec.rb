f5baa12aca7bd91ed420ecf9981c995ac8fd732c
Alexander Koltunov <firstsano@gmail.com>
2019-02-01 14:35:51 +0400

    Add spec for index action

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -7,6 +7,11 @@ RSpec.describe QuestionsController, type: :controller do
       get :index
       expect(assigns(:questions)).to match_array(questions)
     end
+
+    it 'renders index view' do
+      get :index
+      expect(response).to render_template :index
+    end
   end
 
   describe 'GET #new' do
