658d855874f77c004024f194005475076145806c
Alexander Koltunov <firstsano@gmail.com>
2019-02-05 20:00:54 +0400

    Add name to User, fix specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :trackable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :user do
     sequence(:email) { |n| "#{n}_#{Faker::Internet.email}" }
+    name { Faker::Name.name_with_middle }
   end
 end
