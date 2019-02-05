fb2a9585a5c665f6f3835e74cb75f9cc1c0c558a
Alexander Koltunov <firstsano@gmail.com>
2019-02-05 20:45:20 +0400

    Add password so that factory can actually create a user

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,6 @@ FactoryBot.define do
   factory :user do
     sequence(:email) { |n| "#{n}_#{Faker::Internet.email}" }
     name { Faker::Name.name_with_middle }
+    password { Faker::Internet.password }
   end
 end
