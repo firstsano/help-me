ad4d754b6cf6eea4a3ae34939e9c85239899304e
Alexander Koltunov <firstsano@gmail.com>
2019-02-07 20:50:55 +0400

    Add question creation feature

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -19,6 +19,7 @@ class QuestionsController < ApplicationController
     @question = Question.new(question_params)
 
     if @question.save
+      flash[:notice] = 'Question created successfully'
       redirect_to @question
     else
       render :new
