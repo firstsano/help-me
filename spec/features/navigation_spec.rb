1a01b37a1050d59544e525eac094be7c03aae4f5
Alexander Koltunov <firstsano@gmail.com>
2019-02-11 21:03:46 +0400

    temp commit

---
+++ spec/features/navigation_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+feature 'User can navigate through pages', %q{
+  In order to find required information
+  As an user
+  I want to be able to see and use navigation bar
+} do
+
+  given(:user) { create :user }
+
+  def expect_main_menu
+    expect(page).to have_selector 'nav.navbar'
+  end
+
+  scenario 'User can see navigation bar and main links' do
+    visit root_path
+
+    expect_main_menu
+    expect(page).to have_link 'Questons', href: questions_path
+  end
+
+  scenario 'Signed in user sees personal info and controls' do
+    sign_in user
+    visit root_path
+
+    expect_main_menu
+    expect(page).to have_content user.name
+    expect(page).to have_button 'Log out'
+  end
+end
