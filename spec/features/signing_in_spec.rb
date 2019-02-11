6fe6999594af18397db406c8390dec105adba274
Alexander Koltunov <firstsano@gmail.com>
2019-02-11 21:03:46 +0400

    temp commit

--- spec/features/signing_in_spec.rb
+++ spec/features/signing_in_spec.rb
@@ -32,3 +32,22 @@ feature 'User can sign in', %q{
     expect(page).to have_content 'Invalid Email or password.'
   end
 end
+
+feature 'User can sign out', %q{
+  In order to provide safety for my profile
+  As an user
+  I want to be able to sign out
+} do
+
+  given(:user) { create :user }
+
+  scenario 'Signed in user tries to sign out' do
+    sign_in user
+
+    visit root_path
+    click_on 'Log out'
+
+    expect(current_path).to eq root_path
+    expect(page).to have_content 'Signed out successfully.'
+  end
+end
