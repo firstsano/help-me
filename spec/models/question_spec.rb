9664ee2ac9a3527e51029140f8f9a03a8f089b68
Alexander Koltunov <firstsano@gmail.com>
2019-03-06 21:14:20 +0400

    Add spec

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Question, type: :model do
   it { is_expected.to have_many :answers }
   it { is_expected.to belong_to :created_by }
+  it { is_expected.to have_many :attachments }
   it { is_expected.to validate_presence_of :created_by }
   it { is_expected.to validate_presence_of :title }
   it { is_expected.to validate_presence_of :body }
