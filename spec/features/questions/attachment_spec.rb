3c0376a24365d938fae1e3e5da9b38a09d6d800f
Alexander Koltunov <firstsano@gmail.com>
2019-03-10 16:43:46 +0400

    Add story 'user can remove attachment from question'

--- spec/features/questions/attachment_spec.rb
+++ spec/features/questions/attachment_spec.rb
@@ -1,5 +1,18 @@
 require_relative '../features_helper'
 
+def add_attachment(filename)
+  click_on 'Add attachment'
+  last_attachment = find_all('.attachment-fields').last
+  expect(last_attachment).to be_present
+  within(last_attachment) { attach_file 'File', filename }
+end
+
+def prepare_question
+  visit new_question_path
+  fill_in 'Title', with: new_question[:title]
+  fill_in 'Body', with: new_question[:body]
+end
+
 feature 'Adding attachment to question', %q{
   In order to explain a question better
   As an author
@@ -7,32 +20,70 @@ feature 'Adding attachment to question', %q{
 } do
 
   given(:user) { create :user }
+  given(:new_question) { attributes_for :question }
 
   before do
     sign_in user
-    visit questions_path
+    prepare_question
   end
 
   scenario 'User creates a question with multiple attachments', js: true do
-    number_of_attachments = 3
-    new_question = attributes_for :question
-    click_on 'Create question'
-
-    fill_in 'Title', with: new_question[:title]
-    fill_in 'Body', with: new_question[:body]
-
     within('.attachments-form') do
-      number_of_attachments.times do
-        click_on 'Add attachment'
-        last_attachment = find_all('.attachment-fields').last
-        within(last_attachment) { attach_file 'File', Rails.root.join('spec', 'fixtures', 'sample.txt') }
-      end
+      add_attachment 'sample.txt'
+      add_attachment 'sample2.txt'
     end
 
     click_on 'Save'
-    expect(page).to have_link 'sample.txt', count: number_of_attachments
 
-    last_attachment = find_all('.attachment').last
-    expect { within(last_attachment) { click_on 'sample.txt' } }.to change { current_path }
+    question = Question.last
+    expect(current_path).to eq question_path(question)
+    expect(page).to have_link 'sample.txt'
+    expect(page).to have_link 'sample2.txt'
+
+    # check if links work
+    visit question_path(question)
+    expect { click_on 'sample.txt' }.to change { current_path }
+    visit question_path(question)
+    expect { click_on 'sample2.txt' }.to change { current_path }
+  end
+end
+
+feature 'Removing attachment from question', %q{
+  In order to remove falsey attachment
+  As an author
+  I want to be able to delete it
+} do
+
+  given(:user) { create :user }
+  given(:new_question) { attributes_for :question }
+
+  before do
+    sign_in user
+    prepare_question
+    within('.attachments-form') do
+      add_attachment 'sample.txt'
+      add_attachment 'sample2.txt'
+      add_attachment 'sample3.txt'
+    end
+    click_on 'Save'
+  end
+
+  scenario 'User deletes second attachment', js: true do
+    question = Question.last
+    visit question_path(question)
+
+    within('.question-controls') { click_on 'Edit' }
+
+    within('.question-form .attachments-form') do
+      attachment = find_all('.attachment-fields')[1]
+      expect(attachment).to be_present
+      within(attachment) { click_on 'Remove attachment' }
+    end
+
+    click_on 'Save'
+
+    expect(current_path).to eq question_path(question)
+    expect(page).to have_link 'sample.txt'
+    expect(page).to have_link 'sample3.txt'
   end
 end
