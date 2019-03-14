d1b5967f38233c3ccdef12926068670be5b12adf
Alexander Koltunov <firstsano@gmail.com>
2019-03-14 19:15:00 +0400

    Add factories for attachments

--- spec/factories/attachments.rb
+++ spec/factories/attachments.rb
@@ -8,5 +8,13 @@ FactoryBot.define do
       source_path = attachment_path evaluator.filename
       attachment.source = Rack::Test::UploadedFile.new source_path
     end
+
+    factory :answer_attachment do
+      association :attachable, factory: :answer
+    end
+
+    factory :question_attachment do
+      association :attachable, factory: :question
+    end
   end
 end
