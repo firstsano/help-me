0bfeea9efc557043464eee9f61ed5e01bc925539
Alexander Koltunov <firstsano@gmail.com>
2019-03-30 13:28:02 +0400

    Add base story for question's answers live update

--- spec/features/answers/create_spec.rb
+++ spec/features/answers/create_spec.rb
@@ -27,6 +27,28 @@ feature 'User can create an answer to the question', %q{
         expect(current_path).to eq question_path(question)
         expect(page).to have_content answer[:body]
       end
+
+      fscenario 'While one user creates an answer another one can see it appears', js: true do
+        Capybara.using_session('first user') do
+          sign_out :user
+          visit question_path(question)
+          expect(page).not_to have_content answer[:body]
+        end
+
+        Capybara.using_session('second user') do
+          visit question_path(question)
+          within('.question-answer') do
+            fill_in 'answer[body]', with: answer[:body]
+            click_on 'Answer the question'
+          end
+
+          expect(page).to have_content answer[:body]
+        end
+
+        Capybara.using_session('first user') do
+          expect(page).to have_content answer[:body]
+        end
+      end
     end
 
     context 'With invalid attributes' do
