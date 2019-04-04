daffbb084b9ddb03014aa7cc3e79062a1513a671
Alexander Koltunov <firstsano@gmail.com>
2019-04-04 23:52:44 +0400

    Send user to gon

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,13 @@
 class ApplicationController < ActionController::Base
   add_flash_types :success, :error
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_gon_user, if: :user_signed_in?
 
   protected
+
+  def set_gon_user
+    gon.user = current_user.id
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
