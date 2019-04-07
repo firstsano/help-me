254598466e49a06e56741b02926fa1db6cc662c5
Alexander Koltunov <firstsano@gmail.com>
2019-04-07 16:00:41 +0400

    Add Comment model specs

---
+++ spec/models/comment_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe Comment, type: :model do
+  context 'Associations' do
+    it { is_expected.to belong_to :commentable }
+    it { is_expected.to belong_to :author }
+  end
+
+  context 'Validations' do
+    it { is_expected.to validate_presence_of :body }
+    it { is_expected.to validate_presence_of :author }
+    it { is_expected.to validate_presence_of :commentable }
+  end
+end
