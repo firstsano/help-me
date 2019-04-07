53bc6e5643032b679ed6b1f680a974b3a99168bd
Alexander Koltunov <firstsano@gmail.com>
2019-04-07 13:55:23 +0400

    Refactor spec, move push spec to separate file

--- spec/features/answers/create_spec.rb
+++ spec/features/answers/create_spec.rb
@@ -6,13 +6,13 @@ feature 'User can create an answer to the question', %q{
   I want to be able to answer the question
 } do
 
+  given(:user) { create :user }
   given(:question) { create :question }
   given(:answer) { attributes_for :answer }
 
   context 'When user is signed in' do
     before do
       create_list :answer, 5, question: question
-      user = create :user
       sign_in user
       visit question_path(question)
     end
@@ -26,28 +26,6 @@ feature 'User can create an answer to the question', %q{
 
         expect(current_path).to eq question_path(question)
         expect(page).to have_content answer[:body]
-      end
-
-      fscenario 'While one user creates an answer another one can see it appears', js: true do
-        Capybara.using_session('first user') do
-          sign_out :user
-          visit question_path(question)
-          expect(page).not_to have_content answer[:body]
-        end
-
-        Capybara.using_session('second user') do
-          visit question_path(question)
-          within('.question-answer') do
-            fill_in 'answer[body]', with: answer[:body]
-            click_on 'Answer the question'
-          end
-
-          expect(page).to have_content answer[:body]
-        end
-
-        Capybara.using_session('first user') do
-          expect(page).to have_content answer[:body]
-        end
       end
     end
 
@@ -68,13 +46,7 @@ feature 'User can create an answer to the question', %q{
     scenario 'User tries to create an answer to the question' do
       visit question_path(question)
 
-      within('.question-answer') do
-        fill_in 'answer[body]', with: answer[:body]
-        click_on 'Answer the question'
-      end
-
-      expect(current_path).to eq new_user_session_path
-      expect(page).to have_content 'You need to sign in'
+      expect(page).not_to have_selector '.question-answer'
     end
   end
 end
