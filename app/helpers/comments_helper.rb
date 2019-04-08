1d5d2ff83e1f5dc9fcd66cedc23debda0b2e7dbe
Alexander Koltunov <firstsano@gmail.com>
2019-04-08 18:38:29 +0400

    Add helper

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -3,4 +3,8 @@ module CommentsHelper
     resource_path_name = resource.class.name.downcase
     send "comment_#{resource_path_name}_path", resource
   end
+
+  def comments_of(commentable)
+    commentable.comments.select(&:persisted?)
+  end
 end
