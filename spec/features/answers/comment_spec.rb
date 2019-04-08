e584460b47c3e98f76a2b0680ba1d319e00b13e6
Alexander Koltunov <firstsano@gmail.com>
2019-04-08 12:54:41 +0400

    Add comments spec for answer, refactor it for question

--- spec/features/answers/comment_spec.rb
+++ spec/features/answers/comment_spec.rb
@@ -1,13 +1,14 @@
 require_relative '../features_helper'
 
-feature 'User can comment a question', %q{
-  In order to discuss a question
+feature 'User can comment an answer', %q{
+  In order to discuss an answer
   As a user
   I want to be able to leave a comment
 } do
 
   given(:user) { create :user }
-  given!(:question) { create :question }
+  given(:question) { create :question }
+  given!(:answer) { create :answer, question: question }
   given(:comment) { attributes_for :comment }
 
   context 'When user is signed in' do
@@ -18,25 +19,25 @@ feature 'User can comment a question', %q{
 
     context 'With valid comment' do
       scenario 'User creates a comment', js: true do
-        within('.question__comments') do
+        within(".answer[data-answer-id='#{answer.id}'] .answer__comments") do
           fill_in 'comment[body]', with: comment[:body]
           click_on 'Add comment'
         end
 
         expect(current_path).to eq question_path(question)
-        within('.question__comments') { expect(page).to have_content comment[:body], count: 1 }
+        within('.answer__comments') { expect(page).to have_content comment[:body], count: 1 }
       end
     end
 
     context 'With invalid comment' do
       scenario 'User tries to create a comment', js: true do
-        within('.question__comments') do
+        within(".answer[data-answer-id='#{answer.id}'] .answer__comments") do
           fill_in 'comment[body]', with: nil
           click_on 'Add comment'
         end
 
         expect(current_path).to eq question_path(question)
-        within('.question__comments') { expect(page).to have_content "Body can't be blank" }
+        within('.answer__comments') { expect(page).to have_content "Body can't be blank" }
       end
     end
   end
@@ -44,7 +45,7 @@ feature 'User can comment a question', %q{
   context 'When user is not signed in' do
     scenario 'User does not see a form to leave a comment' do
       visit question_path(question)
-      within('.question__comments') { expect(page).not_to have_selector '.comments__form' }
+      within('.answer__comments') { expect(page).not_to have_selector '.comments__form' }
     end
   end
 end
