aa91dc2fd5fc4f5188ffc49e55ac6d03afe8a578
Alexander Koltunov <firstsano@gmail.com>
2019-04-19 18:54:57 +0400

    Add approximate specs for user's model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,4 +7,36 @@ RSpec.describe User, type: :model do
     it { is_expected.to validate_presence_of :name }
     it { is_expected.to validate_presence_of :password }
   end
+
+  context 'Associations' do
+    it { is_expected.to have_many :authorizations }
+  end
+
+  context 'Class methods' do
+    subject { described_class }
+
+    describe '.find_for_oauth' do
+      let!(:user) { create :user }
+      let(:auth) { OmniAuth::AuthHash.new(provider: 'provider', uid: '123456') }
+
+      it { is_expected.to respond_to :find_for_oauth }
+
+      context 'user already has an authorization' do
+        it 'returns the user' do
+          user.authorizations.create(provider: 'provider', uid: '123456')
+          expect(User.find_for_oauth(auth)).to eq user
+        end
+      end
+
+      context 'user has no authorization' do
+        context 'user already exists' do
+
+        end
+
+        context 'user does not exist' do
+
+        end
+      end
+    end
+  end
 end
