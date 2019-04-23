5556f257c1d40d515d022919a10a2d8c5f182976
Alexander Koltunov <firstsano@gmail.com>
2019-04-23 16:12:35 +0400

    Add find_for_oauth method

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,27 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: %i[google_oauth2]
 
+  has_many :authorizations
+
   validates :name, presence: true
 
   def to_s
     name
+  end
+
+  def self.find_for_oauth(auth)
+    provider, uid = auth.provider, auth.uid.to_s
+    authorization = Authorization.find_by provider: provider, uid: uid
+    return authorization.user if authorization
+
+    user = self.find_by(email: auth.info[:email]) || self.create_from_ouath(auth)
+    user.authorizations.create provider: provider, uid: uid
+    user
+  end
+
+  def self.create_from_ouath(auth)
+    password = Devise.friendly_token[0..20]
+    User.create! email: auth.info[:email], name: auth.info[:name],
+                 password: password, password_confirmation: password
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,28 +13,61 @@ RSpec.describe User, type: :model do
   end
 
   context 'Class methods' do
-    subject { described_class }
+    subject(:users) { described_class }
 
     describe '.find_for_oauth' do
-      let!(:user) { create :user }
-      let(:auth) { OmniAuth::AuthHash.new(provider: 'provider', uid: '123456') }
+      let(:email) { Faker::Internet.email }
+      let(:auth) { OmniAuth::AuthHash.new provider: 'provider', uid: '123456', info: { email: email, name: Faker::Name.name } }
 
       it { is_expected.to respond_to :find_for_oauth }
 
       context 'user already has an authorization' do
         it 'returns the user' do
-          user.authorizations.create(provider: 'provider', uid: '123456')
-          expect(User.find_for_oauth(auth)).to eq user
+          user = create :user
+          user.authorizations.create provider: 'provider', uid: '123456'
+          expect(users.find_for_oauth(auth)).to eq user
         end
       end
 
       context 'user has no authorization' do
         context 'user already exists' do
+          let!(:user) { create :user, email: email }
 
+          it 'does not create new user' do
+            expect { users.find_for_oauth(auth) }.not_to change(users, :count)
+          end
+
+          it 'creates an authorization' do
+            expect { users.find_for_oauth(auth) }.to change(user.authorizations, :count).by(1)
+            authorization = user.authorizations.first
+            expect(authorization.provider).to eq auth.provider
+            expect(authorization.uid).to eq auth.uid
+          end
+
+          it 'returns the user' do
+            expect(users.find_for_oauth(auth)).to eq user
+          end
         end
 
         context 'user does not exist' do
+          it 'creates a user' do
+            expect { users.find_for_oauth(auth) }.to change(users, :count).by(1)
+          end
 
+          it 'creates an authorization' do
+            expect { users.find_for_oauth(auth) }.to change(Authorization, :count).by(1)
+          end
+
+          it 'creates an authorization with appropriate provider and uid' do
+            user = users.find_for_oauth(auth)
+            authorization = user.authorizations.first
+            expect(authorization.provider).to eq auth.provider
+            expect(authorization.uid).to eq auth.uid
+          end
+
+          it 'returns the user' do
+            expect(users.find_for_oauth(auth)).to be_an_instance_of(users)
+          end
         end
       end
     end
