7faad390b5fb48f4177af4056fdd961a635cbabe
Alexander Koltunov <firstsano@gmail.com>
2019-04-23 16:26:34 +0400

    Add google oauth specs

--- spec/features/signing/ouath/google_spec.rb
+++ spec/features/signing/ouath/google_spec.rb
@@ -6,8 +6,10 @@ feature 'User can sign in with google', %q{
   I want to be able to sign in with google
 } do
 
-  context 'When user has an account' do
+  context 'When user has an authorization' do
     given!(:user) { create :user }
+    given!(:authorization) { create :authorization, provider: 'google_oauth2', user: user }
+    before { mock_google_hash user: user, authorization: authorization }
 
     scenario 'User signs in' do
       visit new_user_session_path
@@ -15,11 +17,36 @@ feature 'User can sign in with google', %q{
 
       click_on 'Sign in with Google'
       expect(current_path).to eq root_path
-      expect(page).to have_content 'Signed in successfully.'
+      expect(page).to have_content 'Successfully authenticated from Google account'
     end
   end
 
-  context 'When user does not have an account' do
-    scenario 'User signs in'
+  context 'When user has no authorization' do
+    context 'When user exists' do
+      given!(:user) { create :user }
+      before { mock_google_hash user: user }
+
+      scenario 'User signs in and authorization is created' do
+        visit new_user_session_path
+        expect(page).to have_link 'Sign in with Google', href: user_google_oauth2_omniauth_authorize_path
+
+        click_on 'Sign in with Google'
+        expect(current_path).to eq root_path
+        expect(page).to have_content 'Successfully authenticated from Google account'
+      end
+    end
+
+    context 'When user does not exist' do
+      before { mock_google_hash }
+
+      scenario 'User is created and signs in' do
+        visit new_user_session_path
+        expect(page).to have_link 'Sign in with Google', href: user_google_oauth2_omniauth_authorize_path
+
+        click_on 'Sign in with Google'
+        expect(current_path).to eq root_path
+        expect(page).to have_content 'Successfully authenticated from Google account'
+      end
+    end
   end
 end
--- spec/support/omniauth_helper.rb
+++ spec/support/omniauth_helper.rb
@@ -1,14 +1,14 @@
 module OmniauthHelper
-  def mock_google_hash(name:, email:)
+  def mock_google_hash(user: nil, authorization: nil)
+    user = user || build(:user)
+    authorization = authorization || build(:authorization)
     OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new({
-      'provider' => 'google_oauth2',
-      'uid' => '123545',
+      'provider' => authorization.provider,
+      'uid' => authorization.uid,
       'info' => {
-        "name" => name,
-        "email" => email,
+        "name" => user.name,
+        "email" => user.email,
         "email_verified" => true,
-        "first_name" => name,
-        "last_name" => name,
         "image" => "mock_image",
       },
       'credentials' => {
