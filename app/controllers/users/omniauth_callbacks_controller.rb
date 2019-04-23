868c1c473c81c0f70c5e2cc9b241ae70fd7cd513
Alexander Koltunov <firstsano@gmail.com>
2019-04-23 17:11:44 +0400

    Add redirection to failure page in edge cases

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -4,10 +4,18 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     if @user.persisted?
       sign_in_and_redirect @user, event: :authentication
       set_flash_message(:notice, :success, kind: "Google") if is_navigational_format?
+    else
+      redirect_to_failure
     end
   end
 
   def failure
+    redirect_to_failure
+  end
+
+  private
+
+  def redirect_to_failure
     redirect_to root_path, error: 'Unable to sign in with provider'
   end
 end
