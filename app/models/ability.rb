26184f3e2c0cceac08d10cb8fbcc47781255b9ab
Alexander Koltunov <firstsano@gmail.com>
2019-05-16 15:53:29 +0400

    Add abilities for comment, upvote and downvote

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,8 +6,13 @@ class Ability
     return unless user.present?
 
     can :read, :all
+    can :comment, [Question, Answer]
     can :create, [Question, Answer, Comment, Attachment]
     can :update, [Question, Answer], created_by: user
     can :destroy, [Question, Answer], created_by: user
+    can :upvote, [Question, Answer]
+    cannot :upvote, [Question, Answer], created_by: user
+    can :downvote, [Question, Answer]
+    cannot :downvote, [Question, Answer], created_by: user
   end
 end
