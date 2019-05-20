ee1d5688668f6805649e2485d55e8c0fd6140b2c
Alexander Koltunov <firstsano@gmail.com>
2019-05-20 19:15:46 +0400

    Reorganize spec helpers

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -72,16 +72,3 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 end
-
-Shoulda::Matchers.configure do |config|
-  config.integrate do |with|
-    with.test_framework :rspec
-    with.library :rails
-  end
-end
-
-module FactoryBot
-  class SyntaxRunner
-    include CoreHelper
-  end
-end
---
+++ spec/support/factory_bot.rb
@@ -0,0 +1,7 @@
+require_relative './helpers/core_helper.rb'
+
+module FactoryBot
+  class SyntaxRunner
+    include CoreHelper
+  end
+end
