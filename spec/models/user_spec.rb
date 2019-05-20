97f73d2790bfe0d58142b379a72d73da5da7246d
Alexander Koltunov <firstsano@gmail.com>
2019-05-20 19:08:14 +0400

    Refactor spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   context 'Validations' do
     it { is_expected.to validate_presence_of :email }
-    it { is_expected.to validate_uniqueness_of(:email).ignoring_case_sensitivity }
+    it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
     it { is_expected.to validate_presence_of :name }
     it { is_expected.to validate_presence_of :password }
   end
