b40a5c0163639f960ac596ca39bd7e19db31dd2f
Alexander Koltunov <firstsano@gmail.com>
2019-06-06 19:03:46 +0400

    Refactor mailer

--- app/mailers/digest_mailer.rb
+++
@@ -1,6 +0,0 @@
-class DigestMailer < ApplicationMailer
-  def digest(user, questions)
-    @questions = questions
-    mail to: user.email, subject: 'Your digest for today'
-  end
-end
--- spec/mailers/previews/digest_mailer_preview.rb
+++
@@ -1,9 +0,0 @@
-# Preview all emails at http://localhost:3000/rails/mailers/digest_mailer
-class DigestMailerPreview < ActionMailer::Preview
-  # Preview this email at http://localhost:3000/rails/mailers/digest_mailer/digest
-  def digest
-    user = User.first
-    questions = Question.limit(10).all
-    DigestMailer.digest user, questions
-  end
-end
