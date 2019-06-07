b9fb88cdcc92f51d582136886fc21e46b439836e
Alexander Koltunov <firstsano@gmail.com>
2019-06-07 12:02:32 +0400

    Remove author of answer from email receivers

--- app/jobs/notificate_question_subscriber_job.rb
+++ app/jobs/notificate_question_subscriber_job.rb
@@ -2,7 +2,7 @@ class NotificateQuestionSubscriberJob < ApplicationJob
   queue_as :default
 
   def perform(answer)
-    subscribers = answer.question.subscribers
+    subscribers = answer.question.subscribers - [answer.created_by]
     subscribers.each do |subscriber|
       QuestionMailer.answer_added(subscriber, answer).deliver_later
     end
