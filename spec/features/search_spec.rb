2e5fad3cd82f2c91ba7c49d61bb9879e955de281
Alexander Koltunov <firstsano@gmail.com>
2019-06-11 08:29:57 +0400

    Add blank search spec

---
+++ spec/features/search_spec.rb
@@ -0,0 +1,9 @@
+require_relative 'features_helper'
+
+feature 'User can find question/answer/comment/user by using search field', %q{
+  In order to find required entity by text
+  As an authenticated user
+  I want to be able to use the search field
+} do
+  
+end
