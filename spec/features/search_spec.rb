0653dbfa665966e580c8f12850b296777dff6a4e
Alexander Koltunov <firstsano@gmail.com>
2019-07-13 20:50:29 +0400

    Refactor search spec

--- spec/features/search_spec.rb
+++ spec/features/search_spec.rb
@@ -26,7 +26,9 @@ feature 'User can find question/answer/comment/user by using search field', %q{
     given!(:required_comments) { create_list :question_comment, 5, body: "Comment with an apple" }
     given!(:required_users) { create_list :user, 5, name: "Name with an apple" }
 
-    scenario 'User submits query and get results of search' do
+    scenario 'User submits query and get results of search', js: true, with_sphinx: true do
+      sphinx_index
+
       visit root_path
       within('.global-search') do
         fill_in "search", with: "appl"
@@ -35,10 +37,28 @@ feature 'User can find question/answer/comment/user by using search field', %q{
 
       expect(current_path).to eq search_path
 
-      required_questions.each { |question| expect(page).to have_link href: question_path(question) }
-      required_answers.each { |answer| expect(page).to have_link href: question_path(answer.question) }
+      required_questions.each do |question|
+        expect(page).to have_link href: question_path(question)
+        expect(page).to have_content question.title
+        expect(page).to have_content question.body
+      end
+
+      required_answers.each do |answer|
+        expect(page).to have_link href: question_path(answer.question)
+        expect(page).to have_content answer.body
+      end
+
+      required_users.each do |user|
+        expect(page).to have_content user.name
+        expect(page).to have_content user.email
+      end
+
       required_comments.each { |comment| expect(page).to have_content comment.body }
-      required_users.each { |user| expect(page).to have_link user_path(user) }
+
+      matches = all('.match')
+      total_objects = required_questions + required_answers +
+        required_comments + required_users
+      expect(matches.count).to eq total_objects.count
     end
   end
 end
