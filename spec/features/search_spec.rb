da07cff1fa787440e67044984b2d02d3b63ea94c
Alexander Koltunov <firstsano@gmail.com>
2019-07-16 20:50:52 +0400

    Add 'not signed in' spec, add spec for selecting category

--- spec/features/search_spec.rb
+++ spec/features/search_spec.rb
@@ -6,7 +6,7 @@ feature 'User can find question/answer/comment/user by using search field', %q{
   I want to be able to use the search field
 } do
 
-  context 'When user is signed in' do
+  context 'When user is signed in', js: true, with_sphinx: true do
     login_user
 
     background do
@@ -26,39 +26,63 @@ feature 'User can find question/answer/comment/user by using search field', %q{
     given!(:required_comments) { create_list :question_comment, 5, body: "Comment with an apple" }
     given!(:required_users) { create_list :user, 5, name: "Name with an apple" }
 
-    scenario 'User submits query and get results of search', js: true, with_sphinx: true do
-      sphinx_index
+    context 'When using common search field' do
+      scenario 'User submits query and gets results of search' do
+        sphinx_index
 
+        visit root_path
+        within('.global-search') do
+          fill_in "search", with: "appl"
+          click_on 'Search'
+        end
+
+        expect(current_path).to eq search_path
+
+        required_questions.each do |question|
+          expect(page).to have_link href: question_path(question)
+          expect(page).to have_content question.title
+          expect(page).to have_content question.body
+        end
+
+        required_answers.each do |answer|
+          expect(page).to have_link href: question_path(answer.question)
+          expect(page).to have_content answer.body
+        end
+
+        required_users.each do |user|
+          expect(page).to have_content user.name
+          expect(page).to have_content user.email
+        end
+
+        required_comments.each { |comment| expect(page).to have_content comment.body }
+
+        matches = all('.match')
+        total_objects = required_questions + required_answers +
+          required_comments + required_users
+        expect(matches.count).to eq total_objects.count
+      end
+    end
+
+    context 'When using targeting search' do
+      scenario 'User selects type of enitity, submits query and gets results' do
+        sphinx_index
+
+        visit root_path
+        within('.global-search') do
+          fill_in "search", with: "appl"
+          within('.global-search__category') { find("option[value='question']").click }
+          click_on 'Search'
+        end
+
+        expect(current_path).to eq search_path
+      end
+    end
+  end
+
+  context 'When user is not signed in' do
+    scenario 'User cannot see search field' do
       visit root_path
-      within('.global-search') do
-        fill_in "search", with: "appl"
-        click_on 'Search'
-      end
-
-      expect(current_path).to eq search_path
-
-      required_questions.each do |question|
-        expect(page).to have_link href: question_path(question)
-        expect(page).to have_content question.title
-        expect(page).to have_content question.body
-      end
-
-      required_answers.each do |answer|
-        expect(page).to have_link href: question_path(answer.question)
-        expect(page).to have_content answer.body
-      end
-
-      required_users.each do |user|
-        expect(page).to have_content user.name
-        expect(page).to have_content user.email
-      end
-
-      required_comments.each { |comment| expect(page).to have_content comment.body }
-
-      matches = all('.match')
-      total_objects = required_questions + required_answers +
-        required_comments + required_users
-      expect(matches.count).to eq total_objects.count
+      expect(page).not_to have_selector '.global-search'
     end
   end
 end
